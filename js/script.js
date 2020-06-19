0843a45ad25e5830a666c3c2e8c0e0de0133e081
slagrach <slagrach@gmail.com>
2020-06-20 00:47:52 +0300

    Update 19.06.20 0:47

--- js/script.js
+++ js/script.js
@@ -16,25 +16,6 @@ $(document).ready(function () {
             left: "-120%"
         }, "slow");
     })
-    // всплывающая картинка
-    // $('.minimized').click(function (event) {
-    //     var i_path = $(this).attr('src');
-    //     $('body').append('<div id="overlay"></div><div id="magnify"><img src="' + i_path + '"><div id="close-popup"><i></i></div></div>');
-    //     $('#magnify').css({
-    //         left: ($(document).width() - $('#magnify').outerWidth()) / 2,
-    //         // top: ($(document).height() - $('#magnify').outerHeight())/2 upd: 24.10.2016
-    //         top: ($(window).height() - $('#magnify').outerHeight()) / 2
-    //     });
-    //     $('#overlay, #magnify').fadeIn('fast');
-    // });
-
-    // $('body').on('click', '#close-popup, #overlay', function (event) {
-    //     event.preventDefault();
-
-    //     $('#overlay, #magnify').fadeOut('fast', function () {
-    //         $('#close-popup, #magnify, #overlay').remove();
-    //     });
-    // });
     // слайдер
     $('.slider__card').slick({
         arrows: true, // стрелки вкдючены
@@ -116,7 +97,7 @@ $(function () {
         return false;
     })
 })
-//медленно к якорям
+// медленно к якорям
 $(document).ready(function () {
     $(".header__menu").on("click", "a", function (event) {
         event.preventDefault();
@@ -127,7 +108,7 @@ $(document).ready(function () {
         }, 1500);
     });
 });
-
+// fancyBox
 $("[data-fancybox]").fancybox({
     infobar: false,
     arrows: false,
